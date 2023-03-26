5875d8a88712ec4ac301cbd32d8403a52444b65b
Artem Ignatev <art.ser.ignatiev@gmail.com>
2023-03-26 12:57:39 +0200

    update git attributes

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
+    'linebreak-style': 'off',
     '@typescript-eslint/ban-types': [
       'error',
       {
