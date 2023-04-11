d9f54f15a793e24f3ea4b20c932459eb548902d1
Artem Ignatev <art.ser.ignatiev@gmail.com>
2023-04-11 14:09:01 +0200

    Localization: add english support, provide API for other resources

--- application/server/api/src/controllers/LocaleApiController.ts
+++ application/server/api/src/controllers/LocaleApiController.ts
@@ -1,33 +1,80 @@
 import type { Request, Response } from 'express';
-import { MvcController, get, api } from 'mvc-middleware';
-import { Database } from '@food-captain/database';
+import { api, get } from 'mvc-middleware';
 import { Logger } from '@food-captain/server-utils';
+import BaseApiController from './BaseApiController';
 
-const supportedLocales = ['ru'];
+const LOCALE_REGION_SEPARATOR = '-';
+const supportedLocales = ['ru', 'en'];
 
 @api('/locale')
-export default class LocaleApiController extends MvcController {
+export default class LocaleApiController extends BaseApiController {
   constructor(
     protected readonly logger: Logger,
-    private readonly db: Database,
     request: Request,
     response: Response
   ) {
-    super(request, response);
-
-    const message = `${request.method.toUpperCase()}: ${request.url}`;
-    this.logger.info(message);
+    super(logger, request, response);
   }
 
   @get('buttons/:locale')
-  async getButtonsAsync(locale: string) {
+  async getButtonsAsync(localeOrLocaleWithRegion: string) {
+    return this.getLocaleResource(localeOrLocaleWithRegion, 'buttons');
+  }
+
+  @get('common/:locale')
+  async getCommonAsync(localeOrLocaleWithRegion: string) {
+    return this.getLocaleResource(localeOrLocaleWithRegion, 'common');
+  }
+
+  @get('dimension/:locale')
+  async getDimensionAsync(localeOrLocaleWithRegion: string) {
+    return this.getLocaleResource(localeOrLocaleWithRegion, 'dimension');
+  }
+
+  @get('dish/:locale')
+  async getDishAsync(localeOrLocaleWithRegion: string) {
+    return this.getLocaleResource(localeOrLocaleWithRegion, 'dish');
+  }
+
+  @get('ingredient/:locale')
+  async getIngredientAsync(localeOrLocaleWithRegion: string) {
+    return this.getLocaleResource(localeOrLocaleWithRegion, 'ingredient');
+  }
+
+  @get('menu/:locale')
+  async getMenuAsync(localeOrLocaleWithRegion: string) {
+    return this.getLocaleResource(localeOrLocaleWithRegion, 'menu');
+  }
+
+  @get('recipe/:locale')
+  async getRecipeAsync(localeOrLocaleWithRegion: string) {
+    return this.getLocaleResource(localeOrLocaleWithRegion, 'recipe');
+  }
+
+  private getLocaleResource(
+    localeOrLocaleWithRegion: string,
+    resource: string
+  ) {
+    const locale = this.getLocale(localeOrLocaleWithRegion);
+
     if (!supportedLocales.includes(locale)) {
-      const message = `${locale} is not supported locale`;
-      this.logger.warning(message);
-      return this.badRequest(message);
+      return this.localeIsNotSupported(locale);
     }
 
-    const buttons = require(`../locales/${locale}/buttons.json`);
+    const buttons = require(`../locales/${locale}/${resource}.json`);
     return this.ok(buttons);
+  }
+
+  private getLocale(locale: string) {
+    if (locale.includes(LOCALE_REGION_SEPARATOR)) {
+      [locale] = locale.split(LOCALE_REGION_SEPARATOR);
+    }
+    return locale;
+  }
+
+  private localeIsNotSupported(locale: string) {
+    const message = `${locale} is not supported locale`;
+    this.logger.warning(message);
+    return this.badRequest(message);
   }
 }
