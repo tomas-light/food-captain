48a32ae46aaa474b4bd2f8e63744b9891d917d31
Artem Ignatev <art.ser.ignatiev@gmail.com>
2023-04-12 14:35:32 +0200

    localization fix (#17)

--- application/client/api/src/LocaleApi.ts
+++ application/client/api/src/LocaleApi.ts
@@ -1,27 +1,18 @@
 import { metadata } from '@food-captain/client-utils';
 import { ApiBase } from './base/ApiBase';
 
+export type LocaleResource =
+  | 'buttons'
+  | 'common'
+  | 'dimension'
+  | 'dish'
+  | 'ingredient'
+  | 'menu'
+  | 'recipe';
+
 @metadata
 export class LocaleApi extends ApiBase {
-  async getButtonsAsync(locale: string) {
-    return this.get<object>(`/locale/buttons/${locale}`);
-  }
-  async getCommonAsync(locale: string) {
-    return this.get<object>(`/locale/buttons/${locale}`);
-  }
-  async getDimensionAsync(locale: string) {
-    return this.get<object>(`/locale/common/${locale}`);
-  }
-  async getDishAsync(locale: string) {
-    return this.get<object>(`/locale/dimension/${locale}`);
-  }
-  async getIngredientAsync(locale: string) {
-    return this.get<object>(`/locale/dish/${locale}`);
-  }
-  async getMenuAsync(locale: string) {
-    return this.get<object>(`/locale/ingredient/${locale}`);
-  }
-  async getRecipeAsync(locale: string) {
-    return this.get<object>(`/locale/menu/${locale}`);
+  async getAsync(locale: string, resource: LocaleResource) {
+    return this.get<object>(`/locale/${resource}/${locale}`);
   }
 }
